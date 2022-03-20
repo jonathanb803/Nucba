0376917be95ce8313a57c4f340f262d612e2a1a2
Jonathan <joni.24bs@gmail.com>
2022-03-20 15:20:53 -0300

    actualizacion

---
+++ JavaScript/index.js
@@ -0,0 +1,74 @@
+
+// const calculator = (num1, num2, operation) => {
+//     if(!num1 || !num2){
+//         console.log("Ingresa numeros")
+//     }
+
+
+//SUMAR
+// if(operation === "sumar"){
+//     console.log(num1 + num2)
+//     } else if(operation === "restar"){
+//     console.log(num1 - num2)
+//     } else if(operation === "multiplicar"){
+//     console.log(num1 * num2)
+//     } else if(operation === "dividir"){
+//     console.log(num1 / num2)
+//     } else {
+//     console.log("Operación invalida")
+//     }
+// }
+
+// switch(operation){
+//     case 'sumar':
+//         console.log(num1 + num2)
+//         break;
+   
+//     case 'restar':
+//         console.log(num1 - num2)
+//         break;
+    
+//     case 'multiplicar':
+//         console.log(num1 * num2)
+//         break;
+    
+//     case 'dividir':
+//         console.log(num1 / num2)
+//         break;
+
+//     default:
+//         console.log('Operación invalida')
+//     }
+// }   
+
+// calculator(10, 10, "restar")
+// calculator(10, 10, "sumar")
+// calculator(10, 10, "multiplicar")
+// calculator(10, 10, "dividir")
+
+//BUCLES
+// While & For
+
+//WHILE (se ejecta dentro de una función. comparar condición, hasta que no se cumpla, no dejara de funcionar)
+
+// const whileLoop = () => {
+//     let i = 1;
+
+//     while(i < 11){
+//         console.log(i)
+//         i++
+//     }
+// }
+
+// Array [siempre entre corchetes] y los objetos dentro de {}
+
+// const myArray = []
+
+const robot = moves => {
+    let initialPositionY = 0;
+    let initialPositionX = 0;
+
+    for(lei i = 0; i < moves.lenght; i++){
+        if(moves[i] === "U")
+    }
+}
